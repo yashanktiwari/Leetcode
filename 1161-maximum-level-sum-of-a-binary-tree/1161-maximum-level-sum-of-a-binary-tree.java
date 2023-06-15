33f01a96fef69d76899d99082a0c1483cef8f28a
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-06-15 12:13:09 +0530

    Time: 8 ms (96.46%), Space: 46.6 MB (10.97%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -20,8 +20,8 @@ class Solution {
     
     public int bfs(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
-        ArrayList<Integer> list = new ArrayList<>();
         queue.add(root);
+        int x = 0, max = Integer.MIN_VALUE, ans = 0;
         while(!queue.isEmpty()) {
             int size = queue.size();
             int csum = 0;
@@ -31,16 +31,13 @@ class Solution {
                 if(temp.right != null) queue.add(temp.right);
                 csum += temp.val;
             }
-            list.add(csum);
+            if(csum > max) {
+                max = csum;
+                ans = x+1;
+            }
+            x++;
         }
         
-        int max = Integer.MIN_VALUE, ans = 0;
-        for(int i=0; i<list.size(); i++) {
-            if(max < list.get(i)) {
-                max = list.get(i);
-                ans = i+1;
-            }
-        }
         return ans;
     }
 }
