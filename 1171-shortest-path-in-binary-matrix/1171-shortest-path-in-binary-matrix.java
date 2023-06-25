3cb3eddcb12d0573b8e15ba779cff71572d012b9
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-06-26 00:16:14 +0530

    Time: 20 ms (26.94%), Space: 45 MB (59.01%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -2,7 +2,6 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int[] source = {0, 0};
         int[] destination = {grid.length-1, grid[0].length-1};
-        // System.out.println((grid.length-1) + " " + (grid[0].length-1));
         return shortestPath(grid, source, destination);
     }
 
@@ -27,64 +26,18 @@ class Solution {
                 return dist;
             }
 
-            // top
-            if(i-1 >= 0 && grid[i-1][j] == 0) {
-                if(dist + 1 < distance[i-1][j]) {
-                    queue.add(new Pair(i-1, j, dist+1));
-                    distance[i-1][j] = dist + 1;
-                }
-            }
-            // left
-            if(j-1 >= 0 && grid[i][j-1] == 0) {
-                if(dist + 1 < distance[i][j-1]) {
-                    queue.add(new Pair(i, j-1, dist+1));
-                    distance[i][j-1] = dist + 1;
-                }
-            }
-            // down
-            if(i+1 < grid.length && grid[i+1][j] == 0) {
-                if(dist + 1 < distance[i+1][j]) {
-                    queue.add(new Pair(i+1, j, dist+1));
-                    distance[i+1][j] = dist + 1;
-                }
-            }
-            // right
-            if(j+1 < grid[0].length && grid[i][j+1] == 0) {
-                if(dist + 1 < distance[i][j+1]) {
-                    queue.add(new Pair(i, j+1, dist+1));
-                    distance[i][j+1] = dist + 1;
-                }
-            }
+            int[] rows = {-1, 0, 1, 0, -1, -1, 1, 1};
+            int[] cols = {0, -1, 0, 1, 1, -1, -1, 1};
 
-            //top right
-            if(i-1 >= 0 && j+1 < grid[0].length && grid[i-1][j+1] == 0) {
-                if(dist + 1 < distance[i-1][j+1]) {
-                    queue.add(new Pair(i-1, j+1, dist+1));
-                    distance[i-1][j+1] = dist + 1;
-                }
-            }
-
-            //top left
-            if(i-1 >= 0 && j-1 >= 0 && grid[i-1][j-1] == 0) {
-                if(dist + 1 < distance[i-1][j-1]) {
-                    queue.add(new Pair(i-1, j-1, dist+1));
-                    distance[i-1][j-1] = dist + 1;
-                }
-            }
-
-            //bottom right
-            if(i+1 < grid.length && j+1 < grid[0].length && grid[i+1][j+1] == 0) {
-                if(dist + 1 < distance[i+1][j+1]) {
-                    queue.add(new Pair(i+1, j+1, dist+1));
-                    distance[i+1][j+1] = dist + 1;
-                }
-            }
-
-            //bottom left
-            if(i+1 < grid.length && j-1 >= 0 && grid[i+1][j-1] == 0) {
-                if(dist + 1 < distance[i+1][j-1]) {
-                    queue.add(new Pair(i+1, j-1, dist+1));
-                    distance[i+1][j-1] = dist + 1;
+            for(int t=0; t<rows.length; t++) {
+                int newrow = i + rows[t];
+                int newcol = j + cols[t];
+                
+                if(newrow >= 0 && newrow < grid.length && newcol >= 0 && newcol < grid[0].length && grid[newrow][newcol] == 0) {
+                    if(dist + 1 < distance[newrow][newcol]) {
+                        queue.add(new Pair(newrow, newcol, dist+1));
+                        distance[newrow][newcol] = dist + 1;
+                    }
                 }
             }
 
