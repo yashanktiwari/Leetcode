801f07ceed28797d6b1d5e858e73902d9726ec60
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-06-26 16:26:21 +0530

    Time: 55 ms (65.84%), Space: 44.1 MB (51.85%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -4,9 +4,13 @@ class Solution {
     }
 
     public int findPath(int[][] arr) {
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(0, 0, 0));
-        int ans = Integer.MAX_VALUE;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((o1, o2) -> {
+            if(o1.effort > o2.effort) return 1;
+            else if(o1.effort < o2.effort) return -1;
+            else return 0;
+        });
+        pq.add(new Pair(0, 0, 0));
+        int ans = -1;
 
         int distance[][] = new int[arr.length][arr[0].length];
         for(int i=0; i<arr.length; i++) {
@@ -15,14 +19,14 @@ class Solution {
 
         int[] row = {-1, 0, 1, 0};
         int[] col = {0, -1, 0, 1};
-        while(!queue.isEmpty()) {
-            Pair rp = queue.poll();
+        while(!pq.isEmpty()) {
+            Pair rp = pq.poll();
             int i = rp.i;
             int j = rp.j;
             int effort = rp.effort;
 
             if(i == arr.length-1 && j == arr[0].length-1) {
-                ans = Math.min(ans, effort);
+                return effort;
             }
 
             for(int t=0; t<row.length; t++) {
@@ -32,7 +36,7 @@ class Solution {
                 if(nrow >= 0 && nrow < arr.length && ncol >= 0 && ncol < arr[0].length) {
                     if(Math.max(effort, Math.abs(arr[nrow][ncol] - arr[i][j])) < distance[nrow][ncol]) {
                         distance[nrow][ncol] = Math.max(effort, Math.abs(arr[nrow][ncol] - arr[i][j]));
-                        queue.add(new Pair(nrow, ncol, Math.max(effort, Math.abs(arr[nrow][ncol] - arr[i][j]))));
+                        pq.add(new Pair(nrow, ncol, Math.max(effort, Math.abs(arr[nrow][ncol] - arr[i][j]))));
                     }
                 }
             }
