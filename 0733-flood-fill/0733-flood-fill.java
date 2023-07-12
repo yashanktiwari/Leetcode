adc2fde374dd0e53069fa09316f2490ea6574fb5
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-07-12 12:51:46 +0530

    Time: 1 ms (48.08%), Space: 44.4 MB (11.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,60 +1,23 @@
 class Solution {
-        public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-            solve(image, sr, sc, color, image[sr][sc]);
-            return image;
-        }
-
-        public void solve(int[][] arr, int sr, int sc, int color, int initColor) {
-            Queue<Pair> queue = new LinkedList<>();
-            int[][] visited = new int[arr.length][arr[0].length];
-
-            queue.add(new Pair(sr, sc));
-            arr[sr][sc] = color;
-            while(!queue.isEmpty()) {
-                Pair rp = queue.poll();
-
-                int i = rp.i;
-                int j = rp.j;
-
-                if(visited[i][j] == color) {
-                    continue;
-                }
-
-                visited[i][j] = color;
-
-                // Check for all the 4 adjacent neighbours
-                // top
-                if(i-1 >= 0 && arr[i-1][j] == initColor && visited[i-1][j] != color) {
-                    queue.add(new Pair(i-1, j));
-                    arr[i-1][j] = color;
-                }
-
-                // left
-                if(j-1 >=0 && arr[i][j-1] == initColor && visited[i][j-1] != color) {
-                    queue.add(new Pair(i, j-1));
-                    arr[i][j-1] = color;
-                }
-
-                // down
-                if(i+1 < arr.length && arr[i+1][j] == initColor && visited[i+1][j] != color) {
-                    queue.add(new Pair(i+1, j));
-                    arr[i+1][j] = color;
-                }
-
-                // right
-                if(j+1 < arr[0].length && arr[i][j+1] == initColor && visited[i][j+1] != color) {
-                    queue.add(new Pair(i, j+1));
-                    arr[i][j+1] = color;
-                }
-            }
-        }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int checkColor = image[sr][sc];
+        boolean[][] visited = new boolean[image.length][image[0].length];
+        colorFill(image, visited, sr, sc, color, checkColor);
+        return image;
     }
 
-    class Pair {
-        int i;
-        int j;
-        public Pair(int i, int j) {
-            this.i = i;
-            this.j = j;
+    public void colorFill(int[][] image, boolean[][] visited, int sr, int sc, int color, int checkColor) {
+
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || visited[sr][sc] == true || image[sr][sc] != checkColor) return;
+        
+        image[sr][sc] = color;
+        visited[sr][sc] = true;
+
+        int[] r = {-1, 0, 1, 0};
+        int[] c = {0, -1, 0, 1};
+
+        for(int x=0; x<4; x++) {
+            colorFill(image, visited, sr+r[x], sc+c[x], color, checkColor);
         }
     }
+}
