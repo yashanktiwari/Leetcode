b86892eca6486ce0efe92a809d83a60f9f22dbb4
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-07-26 19:28:12 +0530

    Time: 39 ms (72.76%), Space: 54.7 MB (76.65%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,56 +1,75 @@
-public class Trie {
-    class Node {
-        char ch;
-        HashMap<Character, Node> child = new HashMap<>();
-        boolean isTerminal = false;
+class Trie {
+    private Node root;
+    public Trie() {
+        root = new Node();
     }
     
-
-    private Node root;
-
-    public Trie() {
-        Node nn = new Node();
-        nn.ch = '*';
-        this.root = nn;
-    }
-
     public void insert(String word) {
-        Node curr = root;
-        for(int i=0; i<word.length(); i++) {
+        Node temp = root;
+        int n = word.length();
+        for(int i=0; i<n; i++) {
             char ch = word.charAt(i);
-            if(curr.child.containsKey(ch)) {
-                curr = curr.child.get(ch);
-            } else {
-                Node nn = new Node();
-                nn.ch = ch;
-                curr.child.put(ch, nn);
-                curr = nn;
+            if(!temp.containsKey(ch)) {
+                temp.put(ch);
             }
+            temp = temp.get(ch);
         }
-        curr.isTerminal = true;
+        temp.flag = true;
     }
-
+    
     public boolean search(String word) {
-        Node curr = root;
-        for(int i=0; i<word.length(); i++) {
+        Node temp = root;
+        int n = word.length();
+        for(int i=0; i<n; i++) {
             char ch = word.charAt(i);
-            if(!curr.child.containsKey(ch)) {
+            if(!temp.containsKey(ch)) {
                 return false;
             }
-            curr = curr.child.get(ch);
+            temp = temp.get(ch);
         }
-        return curr.isTerminal;
+        return temp.flag == true;
     }
-
-    public boolean startsWith(String word) {
-        Node curr = root;
-        for(int i=0; i<word.length(); i++) {
-            char ch = word.charAt(i);
-            if(!curr.child.containsKey(ch)) {
+    
+    public boolean startsWith(String prefix) {
+        Node temp = root;
+        int n = prefix.length();
+        for(int i=0; i<n; i++) {
+            char ch = prefix.charAt(i);
+            if(!temp.containsKey(ch)) {
                 return false;
             }
-            curr = curr.child.get(ch);
+            temp = temp.get(ch);
         }
         return true;
     }
+    
+    class Node {
+        Node[] arr;
+        boolean flag;
+        
+        public Node() {
+            arr = new Node[26];
+            flag = false;
+        }
+        
+        public void put(char ch) {
+            arr[ch-'a'] = new Node();
+        }
+        
+        public Node get(char ch) {
+            return arr[ch-'a'];
+        }
+        
+        public boolean containsKey(char ch) {
+            return arr[ch-'a'] != null;
+        }
+    }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
