fc9dc4e090cb31c59888eba3df3325925dd36641
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-08-22 12:20:54 +0530

    Time: 147 ms (66.55%), Space: 108.5 MB (89.82%) - LeetHub

--- 1826-maximum-xor-with-an-element-from-array/1826-maximum-xor-with-an-element-from-array.java
+++ 1826-maximum-xor-with-an-element-from-array/1826-maximum-xor-with-an-element-from-array.java
@@ -1,109 +1,121 @@
 class Solution {
-    public int[] maximizeXor(int[] nums, int[][] queries) 
-    {
-        Arrays.sort(nums);
-        Pair[] p = new Pair[queries.length];
-        for (int i = 0; i < p.length; i++)
-        {
-           p[i] = new Pair(queries[i][0],queries[i][1],i);
-        }
-        Arrays.sort(p,new Comparator<Pair>()
-        {
-            @Override
-            public int compare(Pair o1,Pair o2)
-            {
-                return o1.mi - o2.mi;
-            }
-        });
-        Node newNode = new Node();
-        int[] ans =  new int[p.length];
-        int j = 0;
-        for (int i = 0; i < ans.length; i++) {
+    public int[] maximizeXor(int[] nums, int[][] queries) {
+        Trie obj = new Trie();
 
-            while(j < nums.length && nums[j] <= p[i].mi)
-            {
-                Insert(newNode,nums[j]);
-                j++;
+        Arrays.sort(nums);
+
+        Pair[] pairs = new Pair[queries.length];
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
+            return a.max - b.max;
+        });
+
+        int x = 0;
+        for(int[] temp : queries) {
+            int a = temp[0];
+            int b = temp[1];
+            pq.add(new Pair(a, b, x));
+            x++;
+        }
+
+        int[] ans = new int[queries.length];
+        int i = 0;
+        while(!pq.isEmpty()) {
+            Pair rp = pq.poll();
+            
+            int n = rp.n;
+            int max = rp.max;
+            int idx = rp.idx;
+
+            int temp = i;
+            while(temp < nums.length && nums[temp] <= max) {
+                obj.insert(nums[temp]);
+                temp++;
             }
-            if(j == 0)
-            {
-                ans[p[i].qi] = -1;
-            }
-            else
-            {
-                ans[p[i].qi] = getmaxXor(newNode,p[i].xi);
+            i = temp;
+
+            if(max < nums[0]) {
+                ans[idx] = -1;
+            } else {
+                ans[idx] = obj.getMax(n);
             }
 
         }
         return ans;
-        
-        
     }
-    public static class Node {
-        Node zero;
-        Node one;
+}
+
+class Pair {
+    int n;
+    int max;
+    int idx;
+
+    public Pair(int n, int max, int idx) {
+        this.n = n;
+        this.max = max;
+        this.idx = idx;
+    }
+}
+
+class Trie {
+    Node root;
+
+    public Trie() {
+        root = new Node();
     }
 
-    public static void Insert(Node root, int val) {
-        Node curr = root;
-        for (int i = 31; i >= 0; i--) {
-            if ((val & (1 << i)) == 0) {
-                if (curr.zero != null) {
-                    curr = curr.zero;
+    public void insert(int n) {
+        Node temp = root;
+        for(int i=31; i>=0; i--) {
+            int curBit = (n >> i) & 1;
+            if(!temp.containsKey(curBit)) {
+                temp.put(curBit);
+            }
+            temp = temp.get(curBit);
+        }
+    }
+
+    public int getMax(int n) {
+        Node temp = root;
+        int number = 0;
+        for(int i=31; i>=0; i--) {
+            int curBit = (n >> i) & 1;
+            if(curBit == 0) {
+                if(temp.containsKey(1)) {
+                    temp = temp.get(1);
+                    number = number + (1 << i);
                 } else {
-                    Node nn = new Node();
-                    curr.zero = nn;
-                    curr = nn;
+                    temp = temp.get(0);
                 }
             } else {
-                if (curr.one != null) {
-                    curr = curr.one;
+                if(temp.containsKey(0)) {
+                    temp = temp.get(0);
+                    number = number + (1 << i);
                 } else {
-                    Node nn = new Node();
-                    curr.one = nn;
-                    curr = nn;
+                    temp = temp.get(1);
                 }
             }
-
         }
+        return number;
+    }
+}
 
+class Node {
+    Node[] arr;
+
+    public Node() {
+        arr = new Node[2];
     }
 
-    public static int getmaxXor(Node root, int val) {
-        int num = 0;
-        Node curr = root;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (val & (1 << i));
-            if (bit == 0) {
-                if (curr.one != null) {
-                    num += (1 << i);
-                    curr = curr.one;
-                } else {
-                    curr = curr.zero;
-                }
-            } else {
-                if (curr.zero != null) {
-                    num += (1 << i);
-                    curr = curr.zero;
-                } else {
-                    curr = curr.one;
-                }
-            }
-
-        }
-        return num;
-
+    public boolean containsKey(int bit) {
+        return arr[bit] != null;
     }
-    public static class Pair
-    {
-        int xi;
-        int mi;
-        int qi;
-        Pair(int xi,int mi,int qi)
-        {
-            this.mi = mi;
-            this.qi = qi;
-            this.xi = xi;
-        }
+
+    public Node get(int bit) {
+        return arr[bit];
+    }
+
+    public void put(int bit) {
+        arr[bit] = new Node();
     }
 }
