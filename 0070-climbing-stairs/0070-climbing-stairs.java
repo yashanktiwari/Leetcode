0d3910eef15559c5c6e6fc12263f863762e6a86b
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-09-20 23:26:20 +0530

    Time: 0 ms (100%), Space: 39.1 MB (65.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+      int[] arr = new int[n+1];
+      Arrays.fill(arr, -1);
+      arr[0] = 0;
+      return climb(n, arr);
+    }
+
+    public static int climb(int n, int[] arr) {
+      if(n < 0) return 0;
+      if(n == 0) return 1;
+      if(arr[n] != -1) return arr[n];
+
+      return arr[n] = climb(n-1, arr) + climb(n-2, arr);
+    }
+}
