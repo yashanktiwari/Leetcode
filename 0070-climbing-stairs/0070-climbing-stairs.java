45cb8719884d87e3e4973b51d87410dacbfaf941
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-09-22 18:59:36 +0530

    Time: 0 ms (100%), Space: 39.2 MB (38.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,39 @@
 class Solution {
     public int climbStairs(int n) {
-      int[] arr = new int[n+1];
-      Arrays.fill(arr, -1);
-      arr[0] = 0;
-      return climb(n, arr);
+      int[] dp = new int[n+1];
+      Arrays.fill(dp, -1);
+      dp[0] = 0;
+      return climb_t(n);
     }
 
     // Memoization code
-    public static int climb(int n, int[] arr) {
-      if(n < 0) return 0;
-      if(n == 0) return 1;
-      if(arr[n] != -1) return arr[n];
+    public static int climb(int n, int[] dp) {
+      if(n <= 1) return 1;
+      if(dp[n] != -1) return dp[n];
 
-      return arr[n] = climb(n-1, arr) + climb(n-2, arr);
+      return dp[n] = climb(n-1, dp) + climb(n-2, dp);
+    }
+
+    // Tabulation code
+    public static int climb_t(int n) {
+      int[] dp = new int[n+1];
+      dp[0] = 1;
+      dp[1] = 1;
+      for(int i=2; i<=n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+      }
+      return dp[n];
+    }
+
+    // Minimizing the space complexity
+    public static int climb_rsc(int n) {
+      int prev = 1;
+      int curr = 1;
+      for(int i=2; i<=n; i++) {
+        int sum = prev + curr;
+        prev = curr;
+        curr = sum;
+      }
+      return curr;
     }
 }
