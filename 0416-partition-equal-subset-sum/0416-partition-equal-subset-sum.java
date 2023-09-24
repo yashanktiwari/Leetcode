3f203a3906f5191ae83d340825132c39b6209478
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-09-24 11:48:16 +0530

    Time: 55 ms (39.79%), Space: 68.7 MB (8.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -7,9 +7,10 @@ class Solution {
         if((sum & 1) == 1) return false;
         else {
             int n = nums.length;
-            int[][] dp = new int[n+1][(sum>>1)+1];
+            int k = sum >> 1;
+            int[][] dp = new int[n+1][k+1];
             for(int[] arr : dp) Arrays.fill(arr, -1);
-            return (check_m(n-1, (sum>>1), nums, dp) == 1);
+            return (check_t(k, nums) == 1);
         }
     }
 
@@ -45,5 +46,26 @@ class Solution {
 
         if(pick == 1 || notPick == 1) return dp[i][k] = 1;
         else return dp[i][k] = 0;
+    }
+
+    // Tabulation solution
+    public int check_t(int k, int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n+1][k+1];
+
+        for(int i=0; i<=n; i++) dp[i][0] = 1;
+        if(arr[0] <= k) dp[0][arr[0]] = 1;
+
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<=k; j++) {
+                int pick = 0;
+                if(j >= arr[i]) pick = dp[i-1][j-arr[i]];
+                int notPick = dp[i-1][j];
+
+                if(pick == 1 || notPick == 1) dp[i][j] = 1;
+                else dp[i][j] = 0;
+            }
+        }
+        return dp[n-1][k];
     }
 }
