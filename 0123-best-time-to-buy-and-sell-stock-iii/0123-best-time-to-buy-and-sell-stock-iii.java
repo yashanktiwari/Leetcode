681ffa5d253f4ced55b7dd8fbd875dfe17341480
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-09-27 01:10:04 +0530

    Time: 124 ms (17.95%), Space: 78 MB (17.93%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][3];
+        for(int[][] ar : dp) {
+            for(int[] a : ar) Arrays.fill(a, -1);
+        }
+        return calc_m(0, 1, 2, prices, dp);
+    }
+
+    // Recursive solution
+    public int calc_r(int i, int shouldBuy, int k, int[] arr) {
+        if(k == 0) return 0;
+        if(i == arr.length) return 0;
+
+        if(shouldBuy == 1) {
+            int buy = (-1 * arr[i]) + calc_r(i+1, 0, k, arr);
+            int notBuy = calc_r(i+1, 1, k, arr);
+            return Math.max(buy, notBuy);
+        } else {
+            int sell = arr[i] + calc_r(i+1, 1, k-1, arr);
+            int notSell = calc_r(i+1, 0, k, arr);
+            return Math.max(sell, notSell);
+        }
+    }
+
+    // Memoization solution
+    public int calc_m(int i, int shouldBuy, int k, int[] arr, int[][][] dp) {
+        if(k == 0) return 0;
+        if(i == arr.length) return 0;
+        if(dp[i][shouldBuy][k] != -1) return dp[i][shouldBuy][k];
+
+        if(shouldBuy == 1) {
+            int buy = (-1 * arr[i]) + calc_m(i+1, 0, k, arr, dp);
+            int notBuy = calc_m(i+1, 1, k, arr, dp);
+            return dp[i][shouldBuy][k] = Math.max(buy, notBuy);
+        } else {
+            int sell = arr[i] + calc_m(i+1, 1, k-1, arr, dp);
+            int notSell = calc_m(i+1, 0, k, arr, dp);
+            return dp[i][shouldBuy][k] = Math.max(sell, notSell);
+        }
+    }
+}
