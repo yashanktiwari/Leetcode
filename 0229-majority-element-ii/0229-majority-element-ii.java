ff1ec1362165ea339df3ae13ac8f49ada0ea6541
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-10-05 21:54:08 +0530

    Time: 2 ms (98.52%), Space: 47 MB (22.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int x : nums) {
-            map.put(x, map.getOrDefault(x, 0)+1);
-        }
+        int freq1 = 0, freq2 = 0;
+        int num1 = -1, num2 = -1;
 
         int n = nums.length;
-        List<Integer> list = new ArrayList<>();
-        for(int x : map.keySet()) {
-            if(map.get(x) > n/3) list.add(x);
+        for(int i=0; i<n; i++) {
+            if(nums[i] == num1) {
+                freq1++;
+            } else if(nums[i] == num2) {
+                freq2++;
+            } else if(freq1 == 0) {
+                num1 = nums[i];
+                freq1 = 1;
+            } else if(freq2 == 0) {
+                num2 = nums[i];
+                freq2 = 1;
+            } else {
+                freq1--;
+                freq2--;
+            }
         }
+
+        freq1 = 0;
+        freq2 = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == num1) freq1++;
+            else if(nums[i] == num2) freq2++;
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        if(freq1 > n/3) list.add(num1);
+        if(num1 != num2 && freq2 > n/3) list.add(num2);
         return list;
     }
 }
