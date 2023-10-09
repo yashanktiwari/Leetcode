d9838928faf7d40f22bba1759cf03c97db7c607c
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-10-10 00:19:36 +0530

    Time: 0 ms (100%), Space: 44 MB (7.91%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,22 @@
+// Traverse from front and back
+// Apply algorithm similar to kadanes
+// Condn: if(pdt == 0) pdt = 1
+
 class Solution {
     public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE;
         int p = 1;
+        int max = Integer.MIN_VALUE;
         for(int x : nums) {
             p *= x;
-            max = Math.max(p, max);
+            max = Math.max(max, p);
             if(p == 0) p = 1;
         }
+
         p = 1;
         int n = nums.length;
         for(int i=n-1; i>=0; i--) {
             p *= nums[i];
-            max = Math.max(p, max);
+            max = Math.max(max, p);
             if(p == 0) p = 1;
         }
 
